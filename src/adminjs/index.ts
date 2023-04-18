ad0a5f5035d2d916f0fed0bb3d04181adc5568b3
Maisa <maisa.folgueral@gmail.com>
2023-04-18 14:43:02 -0300

    Updated config adminjs

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,74 +3,25 @@ import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { Category, Course, Episode, User } from "../models";
-import bcrypt from "bcrypt";
 import { locale } from "./locale";
+import { dashboardOptions } from "./dashboard";
+import { brandingOptions } from "./branding";
+import { authtenticationOptions } from "./authentication";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
 export const adminJs = new AdminJS({
   databases: [sequelize],
-  rootPath: "/admin",
   resources: adminJsResources,
-  branding: {
-    companyName: "OneBitFlix",
-    logo: "/onebitflix.svg",
-    theme: {
-      colors: {
-        primary100: "#ff0043",
-        primary80: "#ff1a57",
-        primary60: "#ff3369",
-        primary40: "#ff4d7c",
-        primary20: "#ff668f",
-        grey100: "#151515",
-        grey80: "#333333",
-        grey60: "#4d4d4d",
-        grey40: "#666666",
-        grey20: "#dddddd",
-        filterBg: "#333333",
-        accent: "#151515",
-        hoverBg: "#151515",
-      },
-    },
-  },
-  dashboard: {
-    component: AdminJS.bundle("./components/Dashboard"),
-    handler: async (req, res, context) => {
-      const courses = await Course.count();
-      const episodes = await Episode.count();
-      const category = await Category.count();
-      const standardUsers = await User.count({ where: { role: "user" } });
-
-      res.json({
-        'Cursos': courses,
-        'Episódios': episodes,
-        'Categorias': category,
-        'Usuários': standardUsers,
-      });
-    },
-  },
+  rootPath: "/admin",
+  dashboard: dashboardOptions,
   locale: locale,
+  branding: brandingOptions,
 });
 
 export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(
   adminJs,
-  {
-    authenticate: async (email, password) => {
-      const user = await User.findOne({ where: { email } }); // email: email
-
-      if (user && user.role === "admin") {
-        const matched = await bcrypt.compare(password, user.password);
-
-        if (matched) {
-          return user;
-        }
-      }
-
-      return false;
-    },
-    cookiePassword: "senha-do-cookie",
-  },
+  authtenticationOptions,
   null,
   {
     resave: false,
