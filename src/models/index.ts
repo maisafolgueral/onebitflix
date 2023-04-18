483b777de1ddec09c5d509a0cd0c2f5fd868770d
Maisa <maisa.folgueral@gmail.com>
2023-04-18 16:13:23 -0300

    Added get info course

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,8 +5,8 @@ import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
 
-Course.belongsTo(Category);
-Course.hasMany(Episode);
+Course.belongsTo(Category); 
+Course.hasMany(Episode, { as: "episodes" });
 
 Episode.belongsTo(Course);
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
-// src/routes.ts
-
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
+import { coursesController } from "./controllers/coursesController";
 
 const router = express.Router();
 
+// categories
 router.get("/categories", categoriesController.index);
 router.get("/categories/:id", categoriesController.show);
+
+// courses
+router.get("/courses/:id", coursesController.show);
 
 export { router };
