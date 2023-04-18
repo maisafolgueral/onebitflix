ea684de764e834f809fef9b1dbc833b43cfb739c
Maisa <maisa.folgueral@gmail.com>
2023-04-18 14:22:32 -0300

    Added personalization dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,7 +3,7 @@ import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { User } from "../models";
+import { Category, Course, Episode, User } from "../models";
 import bcrypt from "bcrypt";
 import { locale } from "./locale";
 
@@ -32,6 +32,22 @@ export const adminJs = new AdminJS({
         accent: "#151515",
         hoverBg: "#151515",
       },
+    },
+  },
+  dashboard: {
+    component: AdminJS.bundle("./components/Dashboard"),
+    handler: async (req, res, context) => {
+      const courses = await Course.count();
+      const episodes = await Episode.count();
+      const category = await Category.count();
+      const standardUsers = await User.count({ where: { role: "user" } });
+
+      res.json({
+        'Cursos': courses,
+        'Episódios': episodes,
+        'Categorias': category,
+        'Usuários': standardUsers,
+      });
     },
   },
   locale: locale,
