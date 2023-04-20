fe217849f2e92bfd2d386625ddbf6d5f34ada1b2
Maisa <maisa.folgueral@gmail.com>
2023-04-20 14:01:43 -0300

    Updated login with json web token

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -1,5 +1,11 @@
-export const jwtService = {
-    signToken: (payload: string | object | Buffer, expiration: string) => {
+import jwt from "jsonwebtoken";
 
-    }
-}
+const secret = "chave-do-jwt";
+
+export const jwtService = {
+  signToken: (payload: string | object | Buffer, expiration: string) => {
+    return jwt.sign(payload, secret, {
+      expiresIn: expiration,
+    });
+  },
+};
