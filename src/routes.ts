08937c3a8a15d883fa9bfa8c3e39d132658a767c
Maisa <maisa.folgueral@gmail.com>
2023-04-24 17:27:56 -0300

    Added episode progress

--- src/routes.ts
+++ src/routes.ts
@@ -26,6 +26,16 @@ router.get("/courses/:id", ensureAuth, coursesController.show);
 
 // episodes
 router.get("/episodes/stream", ensureAuthViaQuery, episodesController.stream);
+router.get(
+  "/episodes/:id/watchTime",
+  ensureAuth,
+  episodesController.getWatchTime
+);
+router.post(
+  "/episodes/:id/watchTime",
+  ensureAuth,
+  episodesController.setWatchTime
+);
 
 // favorites
 router.get("/favorites", ensureAuth, favoritesController.index);
